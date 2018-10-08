53faf1bdc7af707283e1ad0b8d7f2dc3f2dc3635
zane <pi@0php.net>
2018-10-08 22:12:40 +0800

    初步搭建 HTTP 服务

---
+++ src/helper.lua
@@ -0,0 +1,145 @@
+helper = {}
+
+local config = nil
+
+function helper.getFile(filename)
+    local content = nil
+    if file.exists(filename) then
+        local fd = file.open(filename, "r")
+        if fd then
+            -- read the file content
+            -- max 8192 bytes
+            content = fd:read(8192)
+            -- close file
+            fd:close();fd = nil
+        end
+    end
+    return content
+end
+
+function helper.getConfig(from_file)
+    from_file = from_file or false
+
+    if config ~= nil and from_file == false then
+        return config
+    end
+
+    local fd = file.open("config.json", "r")
+    if fd then
+        -- read the config file content
+        -- max 8192 bytes
+        local config = fd:read(8192)
+        -- close file
+        fd:close();fd = nil
+        -- decode the json content to table and return
+        config = sjson.decode(config)
+        return config
+    end
+
+    return nil
+end
+
+function helper.setConfig(config)
+    local fd = file.open("config.json", "w")
+    if fd then
+        local ok
+        local json
+        -- encode config table to json
+        ok, json = pcall(sjson.encode, config)
+        if not ok then return nil end
+        -- write config json to file
+        ok = fd:write(json)
+        -- close file
+        fd:close();fd = nil
+        -- load new config
+        if ok then
+            helper.getConfig(true)
+        end
+        return ok
+    end
+
+    return nil
+end
+
+function helper.log(...)
+    for i, info in ipairs{...} do
+        local info_type = type(info)
+        if info_type == "table" then
+            for k, v in pairs(info) do
+                print(k, v)
+            end
+        else
+            print(info)
+        end
+    end
+end
+
+function helper.isLogin(table, cookie)
+    if cookie == nil then
+        return false
+    end
+    
+    cookie = cookie:match("PANDA_ID=(.+)")
+    if cookie ~= nil and table[cookie] ~= nil then
+        return true
+    end
+
+    return false
+end
+
+function helper.cookie(len)
+    len = len or 32
+
+    local val = ""
+    for i=1, len do  
+        val = val .. string.char(node.random(65, 90))
+    end
+
+    return val
+end
+
+function helper.okResponse(body, content_type, cookie)
+    content_type = content_type or "text/html"
+    if cookie then
+        cookie = "\r\nSet-Cookie: PANDA_ID=" .. cookie
+    else
+        cookie = ""
+    end
+    body = body or "OK"
+    local response = [[HTTP/1.1 200 OK
+Content-Type: %s%s
+
+%s]]
+    response = string.format(response, content_type, cookie, body)
+    return response
+end
+
+function helper.redirectResponse(url, cookie)
+    if cookie then
+        cookie = "\r\nSet-Cookie: PANDA_ID=" .. cookie
+    else
+        cookie = ""
+    end
+    local response = [[HTTP/1.1 301 Moved Permanently
+Location: %s%s
+
+]]
+    response = string.format(response, url, cookie)
+    return response
+end
+
+function helper.badRequestResponse()
+    return [[HTTP/1.1 400 Bad Request
+
+<h1>400 Bad Request!</h1>
+]]
+end
+
+function helper.notFoundResponse()
+    return [[HTTP/1.1 404 Not Found
+
+<h1>404 Not Found!</h1>
+]]
+end
+
+return helper
---
+++ src/webserver.lua
@@ -0,0 +1,145 @@
+require("helper")
+
+webserver = {}
+
+local server
+
+-- first alpha is upper and others are lower
+local want_headers = {}
+want_headers["Host"] = true
+want_headers["Cookie"] = true
+
+-- files can't be read
+local denny_list = {}
+denny_list["config.json"] = true
+
+local routes = {}
+
+local function tryFileResponse(uri)
+    -- get file name
+    local filename = uri:match("/(.-)%?.*") or uri:match("/(.+)")
+    if filename == nil then
+        return helper.notFoundResponse()
+    end
+    helper.log("request filename: " .. filename)
+    
+    -- protect private file
+    if denny_list[filename] then
+        return helper.badRequestResponse()
+    end
+    -- return file content
+    local content = helper.getFile(filename)
+    if content ~= nil then
+        return helper.okResponse(content)
+    end
+    -- error response
+    helper.log("file not found or can't read:" .. filename)
+    return helper.notFoundResponse()
+end
+
+local function routing(method, uri, headers, body)
+    helper.log("Routing ...")
+    helper.log(method, uri, headers)
+    helper.log(body)
+    for k, route in pairs(routes) do
+        if method == route.method and uri == route.uri then
+            return route.action(headers, body)
+        end
+    end
+    if method == "GET" then
+        return tryFileResponse(uri)
+    end
+    return helper.notFoundResponse()
+end
+
+local function sendResponse(conn, response)
+    conn:send(response, function (c) c:close() end)
+end
+
+local onReceive = function(conn, playload)
+
+    local method  = nil
+    local uri     = nil
+    local version = nil
+    local headers_table = {}
+
+    local request_end = playload:find("\r\n")
+    local headers_end = playload:find("\r\n\r\n")
+
+    if request_end == nil or headers_end == nil then
+        sendResponse(conn, helper.badRequestResponse)
+        return nil
+    end
+
+    local request = playload:sub(1, request_end - 1)
+    local headers = playload:sub(request_end + 2, headers_end + 1)
+    local body    = playload:sub(headers_end+4)
+    -- free playload
+    playload = nil
+
+    method, uri, version = request:match("([^%s]+) ([^%s]+) ([^%s]+)")
+
+    for line in headers:gmatch("(.-)\r\n") do
+        local key
+        local val
+        key, val = line:match("(.-): (.+)")
+        if key ~= nil and val ~= nil and want_headers[key] ~= nil then
+            headers_table[key] = val
+        end
+    end
+    -- free headers
+    headers = nil
+
+    response = routing(method, uri, headers_table, body)
+
+    sendResponse(conn, response)
+end
+
+function webserver.start()
+    if server ~= nil then
+        helper.log("webserver already started!")
+        return
+    end
+
+    server = net.createServer(net.TCP)
+    server:listen(
+        80,
+        function(conn)
+            conn:on("receive", onReceive)
+        end
+    )
+
+    if server ~= nil then
+        helper.log("webserver start success.")
+    else
+        helper.log("fail to start webserver.")
+    end
+end
+
+function webserver.stop()
+    helper.log("stop webserver.")
+    if server ~= nil then
+        server:close()
+        server = nil
+    end
+end
+
+function webserver.restart()
+    webserver.stop()
+    webserver.start()
+end
+
+function webserver.addRoute(method, uri, action)
+    local route = {}
+    route.method = method
+    route.uri    = uri
+    route.action = action
+
+    table.insert(routes, route)
+end
+
+function webserver.clearRoutes()
+    routes = {}
+end
+
+return webserver

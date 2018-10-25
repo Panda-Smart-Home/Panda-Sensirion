5f2e91d610a7b080d5ae113296041ef5d6336f88
zane <pi@0php.net>
2018-10-25 17:35:59 +0800

    增加 LFS 镜像并暂时停止更新

---
+++ build/manage.js
@@ -0,0 +1,127 @@
+var hiddenSwitchList = document.getElementById('hidden-switch').classList;
+var controlSwitchList = document.getElementById('control-switch').classList;
+hiddenSwitch();
+controlSwitch();
+
+function hiddenSwitch() {
+    if (document.getElementById('hidden').checked) {
+        hiddenSwitchList.remove('off')
+        hiddenSwitchList.add('on')
+    } else {
+        hiddenSwitchList.remove('on')
+        hiddenSwitchList.add('off')
+    }
+}
+
+function controlSwitch() {
+    if (document.getElementById('control').checked) {
+        controlSwitchList.remove('off')
+        controlSwitchList.add('on')
+    } else {
+        controlSwitchList.remove('on')
+        controlSwitchList.add('off')
+    }
+}
+
+function clickSwitch(id) {
+    if (id === 'control') {
+        var xhr = new XMLHttpRequest();
+        if (!document.getElementById(id).checked) {
+            xhr.open('GET', '/control/on', false);
+        } else {
+            xhr.open('GET', '/control/off', false);
+        }
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) { 
+                document.getElementById(id).click();
+            } else {
+                alert("控制开关失败，请重试！")
+            }
+        };
+        xhr.send();
+        return;
+    }
+    document.getElementById(id).click();
+}
+
+function changeAp() {
+    var letterNumber = /^[0-9a-zA-Z]+$/;
+    var ap_ssid = document.getElementById("ap-ssid").value;
+    var ap_pwd = document.getElementById("ap-pwd").value;
+    if (!letterNumber.test(ap_ssid) || !letterNumber.test(ap_pwd)) {
+        alert("输入 WIFI 名称或密码必须只包含英文字母或数字，请重新输入!");
+        return false;
+    }
+    if (ap_pwd.length < 8) {
+        alert("密码最低为 8 位，请重新输入！");
+        return false;
+    }
+    return true;
+}
+
+function changeUser() {
+    var letterNumber = /^[0-9a-zA-Z]+$/;
+    var old_username = document.getElementById("old-username").value;
+    var old_password = document.getElementById("old-password").value;
+    var new_username = document.getElementById("new-username").value;
+    var new_password = document.getElementById("new-password").value;
+    if (
+        !letterNumber.test(old_username) 
+        || !letterNumber.test(old_password) 
+        || !letterNumber.test(new_username)
+        || !letterNumber.test(new_password)
+    ) {
+        alert("输入错误，用户名密码只包含英文字母或数字，请重新输入！");
+        return false;
+    }
+    if (new_password.length < 8) {
+        alert("新密码最低为 8 位，请重新输入！");
+        return false;
+    }
+    return true;
+}
+
+function logout() {
+    window.location.href = "http://192.168.26.1/logout"
+}
+
+function reboot() {
+    if (confirm("确定重启？")) {
+        var xhr = new XMLHttpRequest();            
+        xhr.open('GET', '/reboot', true);
+        xhr.send();
+        alert("请等待设备重启后，重新登录！");
+    }
+}
+
+function reset() {
+    if (confirm("确定恢复出厂设置？这将会重置配置并重启！")) {
+        var xhr = new XMLHttpRequest();            
+        xhr.open('GET', '/reset', true);
+        xhr.send();
+        alert("请等待设备重启后，重新登录！");
+    }
+}
+
+function GetQueryString(name)
+{
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); 
+    return null;
+}
+if (GetQueryString("ap") === "fail") {
+    alert("设置设备 WIFI 出错，请检查输入是否正确（合法的WIFI名称和密码）！后重试");
+} else if (GetQueryString("ap") === "ok") {
+    alert("设置成功！设备 WIFI 将会重启，请重新连接设备 WIFI。");
+}
+if (GetQueryString("sta") === "fail") {
+    alert("家庭网络设置失败！");
+} else if (GetQueryString("user") === "ok") {
+    alert("家庭网络设置成功。");
+}
+if (GetQueryString("user") === "fail") {
+    alert("设置用户名密码出错，请检查原用户名密码输入是否正确！");
+} else if (GetQueryString("user") === "ok") {
+    alert("设置成功！下次登录将使用新用户名密码。");
+}
---
+++ build/routes.lua
@@ -0,0 +1,198 @@
+webserver.clearRoutes()
+
+webserver.addRoute("GET", "/", true,
+    function (headers, body, conn)
+        local config = helper.getConfig();
+        local switch = ""
+        if gpio.read(switch_pin) == 1 then
+            switch = "checked"
+        end
+        local hidden = ""
+        if config.ap.hidden then
+            hidden = "checked"
+        end
+        local sta_ip = wifi.sta.getip() or "0.0.0.0"
+        local info = {
+            config.id,
+            config.name,
+            config.chip,
+            switch,
+            config.ap.ssid,
+            hidden,
+            sta_ip,
+            config.sta.ssid,
+            config.username
+        }
+        -- free config
+        config = nil
+        local fd = file.open("manage.html", "r")
+        local rep = table.remove(info, 1)
+        local pos = 0
+        local send_line = function (conn)
+            local line
+            local chunk = ""
+            for i=1, 5 do
+                line = fd:readline()
+                if line ~= nil then
+                    if #info > 0 or rep ~= nil then
+                        line, pos = line:gsub("%$", rep)
+                        if pos > 0 then 
+                            rep = table.remove(info, 1)
+                        end
+                    end
+                    chunk = chunk .. line
+                elseif chunk == "" then
+                    fd:close()
+                    fd = nil
+                    info = nil
+                    conn:close()
+                    return nil
+                end
+            end
+            conn:send(chunk)
+        end
+        conn:on("sent", send_line)
+        conn:send(helper.okHeader())
+        return nil
+    end
+)
+
+webserver.addRoute("GET", "/logout", false,
+    function (headers, body)
+        local status, cookie = helper.isLogin(headers["Cookie"])
+        if cookie ~= nil then
+            helper.clearCookie(cookie)
+        end
+        return helper.redirectResponse("http://192.168.26.1/login.html")
+    end
+)
+
+webserver.addRoute("POST", "/login", false,
+    function (headers, body)
+        local input_username, input_password = body:match("username=([^%s]+)&password=([^%s]+)")
+        local username = helper.getConfig().username
+        local password = helper.getConfig().password
+
+        if username == input_username and password == input_password then
+            local cookie = helper.setCookie()
+            helper.log("set cookie: " .. cookie)
+            return helper.redirectResponse("http://192.168.26.1/", cookie)
+        end
+
+        return helper.redirectResponse("http://192.168.26.1/login.html?status=fail")
+    end
+)
+
+webserver.addRoute("GET", "/control/on", true,
+    function (headers, body)
+        gpio.mode(switch_pin, gpio.OUTPUT)
+        gpio.write(switch_pin, gpio.HIGH)
+        if gpio.read(switch_pin) == 1 then
+            return helper.okHeader() .. "OK"
+        end
+        return helper.badRequestResponse()
+    end
+)
+
+webserver.addRoute("GET", "/control/off", true,
+    function (headers, body)
+        gpio.mode(switch_pin, gpio.OUTPUT)
+        gpio.write(switch_pin, gpio.LOW)
+        if gpio.read(switch_pin) == 0 then
+            return helper.okHeader() .. "OK"
+        end
+        return helper.badRequestResponse()
+    end
+)
+
+webserver.addRoute("POST", "/config/ap", true,
+    function (headers, body)
+        local ssid, pwd = body:match("ssid=([^%s]+)&pwd=([^%s&]+)")
+        local hidden = body:match("[^%s]+&hidden=([^%s]+)")
+        if hidden ~= "on" then
+            hidden = false
+        else
+            hidden = true
+        end
+
+        if helper.minString(ssid) and helper.minString(pwd, 8) then
+            local config = helper.getConfig()
+            config.ap["ssid"]   = ssid
+            config.ap["pwd"]    = pwd
+            config.ap["hidden"] = hidden
+            
+            if helper.setConfig(config) then
+                tmr.start(tmr_tab.ap.id)
+                return helper.redirectResponse("http://192.168.26.1/?ap=ok")
+            end
+        end
+
+        return helper.redirectResponse("http://192.168.26.1/?ap=fail")
+    end
+)
+
+webserver.addRoute("POST", "/config/sta", true,
+    function (headers, body)
+        local ssid, pwd = body:match("ssid=([^%s]+)&pwd=([^%s&]+)")
+
+        local config = helper.getConfig()
+        config.sta["ssid"] = ssid
+        config.sta["pwd"]  = pwd
+
+        if helper.setConfig(config) then
+            sta.setChange(true)
+            tmr.start(tmr_tab.sta.id)
+            udp.reset()
+            return helper.redirectResponse("http://192.168.26.1/?sta=ok")
+        end
+
+        return helper.redirectResponse("http://192.168.26.1/?sta=fail")
+    end
+)
+
+webserver.addRoute("POST", "/config/user", true,
+    function (headers, body)
+        local old_username, old_password, new_username, new_password = body:match("old_username=([^%s]+)&old_password=([^%s]+)&new_username=([^%s]+)&new_password=([^%s]+)")
+        if (old_username ~= helper.getConfig().username)
+            or (old_password ~= helper.getConfig().password)
+            or not helper.minString(new_username)
+            or not helper.minString(new_password, 8)
+        then
+            return helper.redirectResponse("http://192.168.26.1/?user=fail")
+        end
+
+        local config = helper.getConfig()
+        config.username = new_username
+        config.password = new_password
+        if helper.setConfig(config) then
+            return helper.redirectResponse("http://192.168.26.1/?user=ok")
+        end
+        return helper.redirectResponse("http://192.168.26.1/?user=fail")
+    end
+)
+
+webserver.addRoute("GET", "/reboot", true,
+    function (headers, body, conn)
+        conn:send(helper.okHeader() .. "OK",
+            function (conn)
+                conn:close()
+                node.restart()
+            end
+        )
+    end
+)
+
+webserver.addRoute("GET", "/reset", true,
+    function (headers, body, conn)
+        if helper.resetConfig() then 
+            conn:send(helper.okHeader() .. "OK",
+                function (conn)
+                    conn:close()
+                    node.restart()
+                end
+            )
+        else
+            return helper.badRequestResponse()
+        end
+    end
+)
